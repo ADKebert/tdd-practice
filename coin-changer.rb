0ea78760a22a51934b547d982814d984d9cc4bbf
Alan Kebert <adkebert@yahoo.com>
2016-09-02 17:06:55 -0400

    Makes quarters test green

--- coin-changer.rb
+++ coin-changer.rb
@@ -3,7 +3,9 @@ def change(total)
     [total, 0, 0, 0]
   elsif total < 10
     [total % 5, total / 5, 0, 0]
-  else
+  elsif total < 25
     [total % 5, total % 10 / 5, total / 10, 0]
+  else
+    [total % 5, total % 25 % 10 / 5, total % 25 / 10, total / 25]
   end
 end
