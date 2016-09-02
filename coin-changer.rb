fef56f959abd977ca2ab7789091104ae96f400e1
Alan Kebert <adkebert@yahoo.com>
2016-09-02 17:34:00 -0400

    Extracts the coins into more meaningful segments
    I'm not happy with repeating the same work on total for each denomination, and adding additional denominations would probably require editing everything

--- coin-changer.rb
+++ coin-changer.rb
@@ -1,3 +1,22 @@
+# Defining constants for coin denominations
+QUARTER = 25
+DIME = 10
+NICKEL = 5
+PENNY = 1
+
 def change(total)
-  [total % 5, total % 25 % 10 / 5, total % 25 / 10, total / 25]
+  # [total % 5, total % 25 % 10 / 5, total % 25 / 10, total / 25]
+  coins = []
+  coins[3] = total / QUARTER
+  total = total % QUARTER
+
+  coins[2] = total / DIME
+  total = total % DIME
+
+  coins[1] = total / NICKEL
+  total = total % NICKEL
+
+  coins[0] = total
+
+  coins
 end
