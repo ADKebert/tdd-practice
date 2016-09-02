ed25a5e327c90e1496c638a89a012736fd25762d
Alan Kebert <adkebert@yahoo.com>
2016-09-02 19:03:12 -0400

    Makes 20-39 test green

--- roman-numeralifier.rb
+++ roman-numeralifier.rb
@@ -1,21 +1,33 @@
 class RomanNumeralifier
   def numeralify(number)
+    numeral = ""
+
+    if number > 29
+      numeral += "X"
+      number -= 10
+    end
+
+    if number > 19
+      numeral += "X"
+      number -= 10
+    end
+
     if number == 19
-      "XIX"
+      numeral += "XIX"
     elsif number > 14
-      "XV" + "I" * (number - 15)
+      numeral += "XV" + "I" * (number - 15)
     elsif number == 14
-      "XIV"
+      numeral += "XIV"
     elsif number > 9
-      "X" + "I" * (number - 10)
+      numeral += "X" + "I" * (number - 10)
     elsif number == 9
-      "IX"
+      numeral += "IX"
     elsif number == 4
-      "IV"
+      numeral += "IV"
     elsif number > 4
-      "V" + "I"  * (number - 5)
+      numeral += "V" + "I"  * (number - 5)
     else
-      "I" * number
+      numeral += "I" * number
     end
   end
 end
