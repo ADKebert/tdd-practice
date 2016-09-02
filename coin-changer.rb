74dd7fd9c4b68bf58ece8a738576a748ef146abc
Alan Kebert <adkebert@yahoo.com>
2016-09-02 17:01:18 -0400

    Makes dimes test green

--- coin-changer.rb
+++ coin-changer.rb
@@ -1,7 +1,9 @@
 def change(total)
   if total < 5
     [total, 0, 0, 0]
-  else
+  elsif total < 10
     [total % 5, total / 5, 0, 0]
+  else
+    [total % 5, total % 10 / 5, total / 10, 0]
   end
 end
