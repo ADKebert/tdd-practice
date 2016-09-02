630a3895130d98a6053d8d20a70f1d2a980e5893
Alan Kebert <adkebert@yahoo.com>
2016-09-02 19:17:36 -0400

    Refactors code so that 10-19 are more similar to 20-39

--- roman-numeralifier.rb
+++ roman-numeralifier.rb
@@ -17,15 +17,13 @@ class RomanNumeralifier
       number -= 10
     end
 
-    if number == 19
-      numeral += "XIX"
-    elsif number > 14
-      numeral += "XV" + "I" * (number - 15)
-    elsif number == 14
-      numeral += "XIV"
-    elsif number > 9
-      numeral += "X" + "I" * (number - 10)
-    elsif number == 9
+
+    if number > 9
+      numeral += "X"
+      number -= 10
+    end
+
+    if number == 9
       numeral += "IX"
     elsif number == 4
       numeral += "IV"
