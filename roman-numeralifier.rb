96d6a87a4e94c9d1a09c8da2d465e0958041fdc6
Alan Kebert <adkebert@yahoo.com>
2016-09-04 07:20:39 -0400

    didn't work out

--- roman-numeralifier.rb
+++ roman-numeralifier.rb
@@ -1,59 +1,62 @@
 class RomanNumeralifier
+  attr_accessor :numeral, :number
+
+  def initialize
+    @numeral = ""
+  end
+
   def numeralify(number)
-    numeral = ""
-    while number > 999
-      numeral += "M"
-      number -= 1000
+    @number = number
+
+    build_numeral
+    @numeral
+  end
+
+  def build_numeral
+    number = @number
+    if number < 4
+      @numeral += "I" * (number)
+      return
     end
 
-    if number > 899
-      numeral += "CM"
-      number -= 900
+    case number
+    when number > 999
+      @numeral += "M"
+      @number -= 1000
+    when number > 899
+      @numeral += "CM"
+      @number -= 900
+    when number > 499
+      @numeral += "D"
+      @number -= 500
+    when number > 399
+      @numeral += "CD"
+      @number -= 400
+    when number > 99
+      @numeral += "C"
+      @number -= 100
+    when number > 89
+      @numeral += "XC"
+      @number -= 90
+    when number > 49
+      @numeral += "L"
+      @number -= 50
+    when number > 39
+      @numeral += "XL"
+      @number -= 40
+    when number > 9
+      @numeral += "X"
+      @number -= 10
+    when number == 9
+      @numeral += "IX"
+      @number -= 9
+    when number > 4
+      @numeral += "V" + "I" * (number - 5)
+      @number -= number
+    when number == 4
+      @numeral += "IV"
+      @number -= number
     end
-
-    if number > 499
-      numeral += "D"
-      number -= 500
-    end
-
-    if number > 399
-      numeral += "CD"
-      number -= 400
-    end
-
-    if number > 99
-      numeral += "C"
-      number -= 100
-    end
-
-    if number > 89
-      numeral += "XC"
-      number -= 90
-    end
-
-    if number > 49
-      numeral += "L"
-      number -= 50
-    end
-
-    if number > 39
-      numeral += "XL"
-      number -= 40
-    end
-
-    while number > 9
-      numeral += "X"
-      number -= 10
-    end
-
-    if number == 9
-      numeral += "IX"
-    elsif number == 4
-      numeral += "IV"
-    elsif number > 4
-      numeral += "V" + "I"  * (number - 5)
-    else
-      numeral += "I" * number
-    end
+    build_numeral
   end
 end
