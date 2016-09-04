ad0488baafb6cf486873d2e41a002340c1c4c411
Alan Kebert <adkebert@yahoo.com>
2016-09-04 11:45:08 -0400

    Add substring test and red same word test

--- anagramsTest.rb
+++ anagramsTest.rb
@@ -9,4 +9,12 @@ describe "Anagrams" do
   it "should return an anagram if there is one" do
     assert_equal ["tan"], anagrams("ant", %w{hot tan tat tart})
   end
+
+  it "shouldn't return an anagram for a substring" do
+    assert_equal [], anagrams("ant", %w{hot tangy tat tart})
+  end
+
+  it "shouldn't match the word itself" do
+    assert_equal ["tan"], anagrams("ant", %w{hot tan ant tart})
+  end
 end
