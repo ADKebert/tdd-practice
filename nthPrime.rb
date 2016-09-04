2275844372013b1f0db015d4a28fa491da80887a
Alan Kebert <adkebert@yahoo.com>
2016-09-04 11:10:05 -0400

    Makes n = 3 test green
    the simplest thing would have been to add 5 to the primes array, but I felt at this point it was obvious that we dont' want to hard code 10k+ primes into an array and that it was time to figure out how to generate a new one

--- nthPrime.rb
+++ nthPrime.rb
@@ -1,6 +1,21 @@
 class NthPrime
   def nth_prime(n)
     primes = [2,3]
+    next_num = primes.last + 1
+    until primes.length >= n do
+      is_prime = true
+
+      primes.each do |prime|
+        if next_num % prime == 0
+          is_prime = false
+        end
+      end
+
+      if is_prime
+        primes << next_num
+      end
+      next_num += 1
+    end
     primes[n - 1]
   end
 end
