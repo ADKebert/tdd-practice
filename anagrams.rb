0a0511b0abcf35693828d2c7847d7c1d18a06057
Alan Kebert <adkebert@yahoo.com>
2016-09-04 11:32:48 -0400

    Makes initial test green

---
+++ anagrams.rb
@@ -0,0 +1,3 @@
+def anagrams(word, potential_anagrams)
+  []
+end
--- anagramsTest.rb
+++ anagramsTest.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require_relative "anagrams"
 
-describe Anagrams do
+describe "Anagrams" do
   it "should return [] if there are no anagrams" do
     assert_equal [], anagrams("ant", %w{hot pot tat tart})
   end
