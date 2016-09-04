9a5b21ade8be3145f47ccfa87adabacf6fdd1d2e
Alan Kebert <adkebert@yahoo.com>
2016-09-04 11:37:11 -0400

    Add red single anagram test

--- anagramsTest.rb
+++ anagramsTest.rb
@@ -5,4 +5,8 @@ describe "Anagrams" do
   it "should return [] if there are no anagrams" do
     assert_equal [], anagrams("ant", %w{hot pot tat tart})
   end
+
+  it "should return an anagram if there is one" do
+    assert_equal ["tan"], anagrams("ant", %w{hot tan tat tart})
+  end
 end
