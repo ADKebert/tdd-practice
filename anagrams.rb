b4027413c876db49e958b52040471a1b569ad4ea
Alan Kebert <adkebert@yahoo.com>
2016-09-04 11:46:27 -0400

    Make same word test green

--- anagrams.rb
+++ anagrams.rb
@@ -1,6 +1,8 @@
 def anagrams(word, potential_anagrams)
   letters = word.split("").sort
+  
   potential_anagrams.select do |potential_anagram|
+    potential_anagram != word &&
     potential_anagram.split("").sort == letters
   end
 end
