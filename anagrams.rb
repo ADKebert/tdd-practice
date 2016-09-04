dd7a31eb96bb5d20980fd7d62222de604a1ff4b5
Alan Kebert <adkebert@yahoo.com>
2016-09-04 11:42:40 -0400

    Makes single anagram test green

--- anagrams.rb
+++ anagrams.rb
@@ -1,3 +1,6 @@
 def anagrams(word, potential_anagrams)
-  []
+  letters = word.split("").sort
+  potential_anagrams.select do |potential_anagram|
+    potential_anagram.split("").sort == letters
+  end
 end
