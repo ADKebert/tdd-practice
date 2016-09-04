b4aafc909eb478832debc2cdfa7a397458b1fcb4
Alan Kebert <adkebert@yahoo.com>
2016-09-04 11:30:03 -0400

    Add first anagrams test

---
+++ anagramsTest.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require_relative "anagrams"
+
+describe Anagrams do
+  it "should return [] if there are no anagrams" do
+    assert_equal [], anagrams("ant", %w{hot pot tat tart})
+  end
+end
