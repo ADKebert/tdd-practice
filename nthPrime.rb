af2cbaf5aec7a0fc08ac68c4c82948c33abb38c0
Alan Kebert <adkebert@yahoo.com>
2016-09-04 11:21:42 -0400

    sped it up by an order of magnitude
    now only considers odd numbers and breaks out of the each loop as soon as a number evenly divides it

--- nthPrime.rb
+++ nthPrime.rb
@@ -1,21 +1,24 @@
 class NthPrime
   def nth_prime(n)
     primes = [2,3]
-    next_num = primes.last + 1
+    next_num = primes.last + 2
     until primes.length >= n do
       is_prime = true
 
       primes.each do |prime|
         if next_num % prime == 0
           is_prime = false
+          break
         end
       end
 
       if is_prime
         primes << next_num
       end
-      next_num += 1
+
+      next_num += 2
     end
+    
     primes[n - 1]
   end
 end
